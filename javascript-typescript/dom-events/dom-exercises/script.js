4ee4fcae0552bd54afae6a4fb6a7a9687bd78167
aikkyy <carolina.pestana.aguiar@gmail.com>
2023-11-22 21:52:28 +0000

    dom exercises solution fix

--- javascript-typescript/dom-events/dom-exercises/script.js
+++ javascript-typescript/dom-events/dom-exercises/script.js
@@ -1,25 +1,25 @@
-// cached DOM elements
+// exercise 1 - change text
 const title = document.getElementById("title");
 const changeButton = document.getElementById("change-btn");
-const listForm = document.getElementById("list-form");
-const inputValue = document.getElementById("input-value");
-const list = document.getElementById("list");
-const printInput = document.getElementById("print-input");
-const mouseOverElement = document.getElementById("mouseover");
 
-// exercise 1 - change text
 function changeText() {
   title.innerText = "Title Changed!";
   changeButton.innerText = "Clicked";
 }
 
+changeButton.addEventListener("click", changeText);
+
 // exercise 2 - add list item
+const listForm = document.getElementById("list-form");
+const inputValue = document.getElementById("input-value");
+const list = document.getElementById("list");
+
 function addListItem(event) {
   event.preventDefault();
 
   let value = inputValue.value;
   if (value === "") {
-    alert("Please add a task");
+    alert("Please add a task!");
     return;
   }
 
@@ -31,6 +31,8 @@ function addListItem(event) {
   inputValue.value = "";
 }
 
+listForm.addEventListener("submit", addListItem);
+
 // exercise 3 - remove list item
 list.addEventListener("click", function (event) {
   if (event.target.classList.contains("list-item")) {
@@ -40,23 +42,28 @@ list.addEventListener("click", function (event) {
 });
 
 // exercise 4 - display input value
+const printInput = document.getElementById("print-input");
+
 inputValue.addEventListener("input", function () {
   printInput.innerText = inputValue.value;
 });
 
 // exercise 5 - toggle background color
+const mouseOverElement = document.getElementById("mouseover");
+
 function toggleBackgroundColor(isOriginalColor) {
-  mouseOverElement.style.backgroundColor = isOriginalColor
-    ? "darksalmon"
-    : "salmon";
+  if (isOriginalColor) {
+    mouseOverElement.style.backgroundColor = "darksalmon";
+  } else {
+    mouseOverElement.style.backgroundColor = "salmon";
+  }
 }
 
-// event listeners
-changeButton.addEventListener("click", changeText);
-listForm.addEventListener("submit", addListItem);
-mouseOverElement.addEventListener("mouseover", () =>
-  toggleBackgroundColor(false)
-);
-mouseOverElement.addEventListener("mouseleave", () =>
-  toggleBackgroundColor(true)
-);
+function onMouseOver() {
+  toggleBackgroundColor(false);
+}
+function onMouseLeave() {
+  toggleBackgroundColor(true);
+}
+mouseOverElement.addEventListener("mouseover", onMouseOver);
+mouseOverElement.addEventListener("mouseleave", onMouseLeave);
