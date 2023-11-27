d07c9a546a1205243728ae79b0d2b751b78d786a
aikkyy <carolina.pestana.aguiar@gmail.com>
2023-11-27 22:49:09 +0000

    ecma demos

--- javascript-typescript/ecma-script/es5/array-methods.js
+++ javascript-typescript/ecma-script/es5/array-methods.js
@@ -3,3 +3,15 @@ var squares = arr.map(function (x) {
   return x * x;
 });
 console.log(squares); // [1, 4, 9, 16, 25]
+
+const someReturn = arr.some((item) => item === 6);
+console.log("some", someReturn); // false
+
+const everyReturn = arr.every((item) => item === 2);
+console.log("every", everyReturn); // false
+
+const findReturn = arr.find((item) => item === 1);
+console.log("find", findReturn); // [1]
+
+const filterReturn = arr.filter((item) => item % 2 === 0);
+console.log("filter", filterReturn); // [2,4]
