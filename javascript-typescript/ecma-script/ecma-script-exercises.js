2a5a5deaac1a2e0b85316e64457cd07e38573bcb
aikkyy <carolina.pestana.aguiar@gmail.com>
2023-11-29 19:44:02 +0000

    ECMAScript exercises update

--- javascript-typescript/ecma-script/ecma-script-exercises.js
+++ javascript-typescript/ecma-script/ecma-script-exercises.js
@@ -3,12 +3,20 @@ const greet = (name) => `Hello, ${name}!`;
 console.log(greet("John"));
 
 // exercise 2
-async function fetchData() {
+async function fetchDataAsync() {
   let response = await fetch("https://random-data-api.com/api/v2/users");
   let data = await response.json();
   console.log(data);
 }
-fetchData();
+fetchDataAsync();
+
+function fetchDataPromise() {
+  fetch("https://random-data-api.com/api/v2/users")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
+fetchDataPromise();
 
 // exercise 3
 let user = {
@@ -17,4 +25,4 @@ let user = {
     street: "123 Main St",
   },
 };
-console.log(user?.address?.street);
+console.log(user.address?.street);
